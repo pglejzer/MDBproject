2b03e30e5a6b5748228b12069b945735fd3d091c
Piotr Glejzer <glejzerpiotr@gmail.com>
2018-06-06 09:49:15 +0200

    project for MDB, not finish yet

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+new WOW().init();
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+window.onscroll = function () {
+    var buttonTop = document.getElementById('button__footer');
+    var scrollAxis = window.pageYOffset;
+
+    if (scrollAxis > '650') {
+        buttonTop.style.display = 'block';
+    } else {
+        buttonTop.style.display = 'none';
+    }
+};
