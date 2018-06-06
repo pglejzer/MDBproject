850d4730065c30b6824d864afbcfaf80fb937696
Piotr Glejzer <glejzerpiotr@gmail.com>
2018-06-07 01:09:56 +0200

    added colors

--- src/js/script.js
+++ src/js/script.js
@@ -3,25 +3,12 @@ new WOW().init();
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
-
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
 });
-
-window.onscroll = function () {
-    var buttonTop = document.getElementById('button__footer');
-    var scrollAxis = window.pageYOffset;
-
-    if (scrollAxis > '650') {
-        buttonTop.style.display = 'block';
-    } else {
-        buttonTop.style.display = 'none';
-    }
-};
-
 $(function () {
     var nav = $(".navbar-collapse");
     nav.on("click", "a:not([data-toggle])", null, function () {
@@ -33,3 +20,13 @@ $('.navbar').on('click', (event) => {
     $(event.target).siblings('.collapse')
         .toggleClass('.nav-link');
 });
+window.onscroll = function () {
+    var buttonTop = document.getElementById('button__footer');
+    var scrollAxis = window.pageYOffset;
+
+    if (scrollAxis > '650') {
+        buttonTop.style.display = 'block';
+    } else {
+        buttonTop.style.display = 'none';
+    }
+};
